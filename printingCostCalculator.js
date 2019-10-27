72228542c08ed893909433aba044e2355ccc4e63
itsdanielguerrero <itsdanielguerrero@gmail.com>
2019-10-27 14:03:48 -0400

    Starting base for OPT Project

---
+++ printingCostCalculator.js
@@ -0,0 +1,18 @@
+ /* ----------------------------------------------------------------
+ *  OPT Industry Project - Team No Sleep
+ *  Purpose - This program will calculate the price of printing a 3d object.
+ *  objectives Create a web application. 
+ *             Allow the user to upload and view a 3D model (.stl-file).
+ *             Calculate key values of the model (volume, material cost etc.).
+ *             Provide a PDF export option with a comprehensive report of cost calculation.
+ *  
+ *  author - 
+ *  author - 
+ *  author - 
+ *  author - Daniel Guerrero - gitHub - https://github.com/itsdanielguerrero
+ * 
+ *  -----------------------------------------------------------------*/
+
+function calculatePrintingPrice(){
+    //not implemented yet
+}
