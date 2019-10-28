174699ff857dad4cf1368b45c44ad54966c9b61b
itsdanielguerrero <55259595+itsdanielguerrero@users.noreply.github.com>
2019-10-28 14:14:11 -0400

    Update printingCostCalculator.js

--- printingCostCalculator.js
+++ printingCostCalculator.js
@@ -6,10 +6,10 @@
  *             Calculate key values of the model (volume, material cost etc.).
  *             Provide a PDF export option with a comprehensive report of cost calculation.
  *  
- *  author - 
- *  author - 
- *  author - 
- *  author - Daniel Guerrero - gitHub - https://github.com/itsdanielguerrero
+ *  author - Madelyn
+ *  author - Keefe
+ *  author - Kullen - https://github.com/Kullen7117
+ *  author - Daniel Guerrero - https://github.com/itsdanielguerrero
  * 
  *  -----------------------------------------------------------------*/
 
